98fd910ce929483a04105417f7a53ee29e969cd8
Aong noppol Nunpiang <nopphol1122@gmail.com>
2023-08-09 18:56:27 +0700

    S

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,8 +28,8 @@ export default function RootLayout({ children }) {
           {children}
           <Footer
             year="2023"
-            fullName="Chayanin Suatap"
-            studentId="650610560"
+            fullName="Noppol Nunpiang"
+            studentId="650612086"
           />
         </div>
       </body>
